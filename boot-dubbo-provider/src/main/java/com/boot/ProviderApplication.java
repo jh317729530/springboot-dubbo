9ad33b183992a4c539a498f3fe9b92ec4fff9943
ganjunhui <junhui_g@gzleihou.cn>
2018-12-12 15:19:50 +0800

    配置服务提供者为非web应用

--- boot-dubbo-provider/src/main/java/com/boot/ProviderApplication.java
+++ boot-dubbo-provider/src/main/java/com/boot/ProviderApplication.java
@@ -1,21 +1,15 @@
 package com.boot;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
-
-import java.io.IOException;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@ImportResource({"classpath:config/spring-dubbo.xml"})
 public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProviderApplication.class, args);
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new SpringApplicationBuilder(ProviderApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
